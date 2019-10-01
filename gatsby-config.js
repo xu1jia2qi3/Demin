1ed40888eb69241ff4dea78c339abde78b43ea79
xu1jia2qi3 <jiaqy@live.com>
2019-10-01 10:32:10 -0400

    add photos and change names

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,29 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
+    // Default title of the page
     siteTitleAlt: `Demin-Welcome to My Studio`,
+    // Used for the title template on pages other than the index site
+    siteTitle: `Demin`,      
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Demin-Studio`,    
+    // Used for SEO
+    siteDescription: `Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI. Includes Light/Dark mode.` 
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-emilia`,
-      options: {},
+      options: {
+        name: `Demin Yin`,
+        location:`Waterloo,ON,Canada`,
+        showThemeAuthor:false,
+        socialMedia: [
+          {
+            title: `Facebook`,
+            href: `https://www.facebook.com/profile.php?id=100009974165746`
+          }
+        ]
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
